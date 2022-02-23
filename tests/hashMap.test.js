246a2add0f92ab6f6b14c173f6a26f6f1a5f5cd7
Fabio Percivaldi <fabio.percivaldi@mia-platform.eu>
2022-02-23 23:03:38 +0100

    added test on multiple consecutive set

--- tests/hashMap.test.js
+++ tests/hashMap.test.js
@@ -21,5 +21,17 @@ tap.test('HashMap - set', test => {
     testCase.equal(actualResult, expectedResult)
     testCase.end()
   })
+
+  test.test('set two different values', testCase => {
+    const expectedResult1 = 'value1'
+    const expectedResult2 = 'value2'
+
+    const actualResult1 = HashMap.set('key', 'value1')
+    const actualResult2 = HashMap.set('key', 'value2')
+
+    testCase.equal(actualResult1, expectedResult1)
+    testCase.equal(actualResult2, expectedResult2)
+    testCase.end()
+  })
   test.end()
 })
