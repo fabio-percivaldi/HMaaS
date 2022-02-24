2d0b7f9f456e7ff2a17ffdd7b14632f56d7dd349
Fabio Percivaldi <fabio.percivaldi@mia-platform.eu>
2022-02-24 20:37:40 +0100

    added swagger and validation on request body

--- index.js
+++ index.js
@@ -2,13 +2,18 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
+
 const app = express()
 const port = 3000
 const logger = require('pino')()
 const routes = require('./routes')
 
-app.use(routes)
 app.use(bodyParser.json())
+app.use(routes)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
   logger.info(`Example app listening on port ${port}`)
--- routes.js
+++ routes.js
@@ -5,15 +5,25 @@ const router = express.Router()
 const HashMap = require('./src/hashMap.js')
 const hashMap = new HashMap()
 const logger = require('pino')()
+const { body: bodyValidator, validationResult } = require('express-validator')
 
-router.post('/set', (req, res) => {
-  const { body } = req
-  logger.info('Request body', JSON.stringify(body))
-  const { key, value } = body
-  hashMap.set(key, value)
+router.post('/set',
+  bodyValidator('key').isString()
+    .notEmpty(),
+  bodyValidator('value').isString()
+    .notEmpty(),
+  (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    const { body } = req
+    logger.info('Request body', JSON.stringify(body))
+    const { key, value } = body
+    hashMap.set(key, value)
 
-  res.status(204).send()
-})
+    res.status(204).send()
+  })
 
 router.get('/get/:key', (req, res) => {
   const value = hashMap.get(req.params.key)
