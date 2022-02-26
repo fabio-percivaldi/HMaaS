e1e22856ff7f2f5578d1b4aebe069e77e60ab14f
Fabio Percivaldi <fabio.percivaldi@mia-platform.eu>
2022-02-26 22:17:46 +0100

    added counter for get/set by user

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const bodyParser = require('body-parser')
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./swagger.json')
 const basicAuth = require('express-basic-auth')
+
+// PROMETHEUS setup
+const client = require('prom-client')
+const { Counter, register } = client
+
+const getCounter = new Counter({
+  name: 'get_item',
+  help: 'User request a Get on an item',
+  labelNames: ['user'],
+})
+const setCounter = new Counter({
+  name: 'set_item',
+  help: 'User request a Set on an item',
+  labelNames: ['user'],
+})
+// REDIS setup
 const { createClient } = require('redis')
 const {
   REDIS_URL,
@@ -18,10 +34,10 @@ const port = 3000
 const logger = require('pino')()
 const routesBuilder = require('./routes')
 
-routesBuilder(redisClient).then(routes => {
+routesBuilder(redisClient, register, getCounter, setCounter).then(routes => {
   app.use(bodyParser.json())
   app.use(basicAuth({
-    users: { 'user': 'password' },
+    users: { 'user': 'password', 'user2': 'password2' },
   }))
   app.use(routes)
 
